c079039c9b8e430932ecd9640a29244262365d82
Stayhye <165840599+Stayhye@users.noreply.github.com>
2026-09-20 15:54:51 -0500

    Update file.h

--- mednafen/file.h
+++ mednafen/file.h
@@ -2,16 +2,27 @@
 #define MDFN_FILE_H
 
 #include <string>
+#include <stdint.h>
 
-#define MDFNFILE_EC_NOTFOUND	1
-#define MDFNFILE_EC_OTHER	2
+#ifndef INLINE
+#if defined(_MSC_VER)
+#define INLINE __inline
+#elif defined(__GNUC__)
+#define INLINE __inline__
+#else
+#define INLINE inline
+#endif
+#endif
+
+#define MDFNFILE_EC_NOTFOUND    1
+#define MDFNFILE_EC_OTHER    2
 
 struct MDFNFILE
 {
- char    *ext;
- uint8_t *data;
- int64_t size;
- int64_t location;
+   char    *ext;
+   uint8_t *data;
+   int64_t size;
+   int64_t location;
 };
 
 struct MDFNFILE *file_open(const char *path);
@@ -19,42 +30,39 @@ int file_close(struct MDFNFILE *file);
 
 class PtrLengthPair
 {
- public:
+public:
 
- inline PtrLengthPair(const void *new_data, const uint64 new_length)
- {
-  data = new_data;
-  length = new_length;
- }
+   inline PtrLengthPair(const void *new_data, const uint64_t new_length)
+   {
+      data = new_data;
+      length = new_length;
+   }
 
- ~PtrLengthPair() 
- { 
+   ~PtrLengthPair() 
+   { 
 
- } 
+   } 
 
- INLINE const void *GetData(void) const
- {
-  return(data);
- }
+   INLINE const void *GetData(void) const
+   {
+      return(data);
+   }
 
- INLINE uint64 GetLength(void) const
- {
-  return(length);
- }
+   INLINE uint64_t GetLength(void) const
+   {
+      return(length);
+   }
 
- private:
- const void *data;
- uint64 length;
+private:
+   const void *data;
+   uint64_t length;
 };
 
 #include <vector>
 
 // These functions should be used for data like save states and non-volatile backup memory.
-// Until(if, even) we add LoadFromFile functions, for reading the files these functions generate, just use gzopen(), gzread(), etc.
-// "compress" is set to the zlib compression level.  0 disables compression entirely, and dumps the file without a gzip header or footer.
-// (Note: There is a setting that will force compress to 0 in the internal DumpToFile logic, for hackers who don't want to ungzip save files.)
-
-bool MDFN_DumpToFile(const char *filename, int compress, const void *data, const uint64 length);
+// "compress" is set to the zlib compression level. 0 disables compression entirely.
+bool MDFN_DumpToFile(const char *filename, int compress, const void *data, const uint64_t length);
 bool MDFN_DumpToFile(const char *filename, int compress, const std::vector<PtrLengthPair> &pearpairs);
 
 #endif
